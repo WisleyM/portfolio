9aaa89519ec6e6716fcea1c1a23f50bec2c2cf13
Wisley Mozer <156000537+WisleyM@users.noreply.github.com>
2024-02-29 18:41:14 +0000

    update 29/02/24
    style mobile

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 const carrocelContent = document.querySelector("#carrocel");
 
-const avancarScroll= (direcao) => {
+const avancarScroll = (direcao) => {
 
 
-    if(direcao == 'left'){
-      carrocelContent.scrollLeft -= 100;
-     
-    }else if (direcao == 'right'){
-      carrocelContent.scrollLeft += 100;
-    
-    }
+  if (direcao == 'left') {
+    carrocelContent.scrollLeft -= 100;
+
+  } else if (direcao == 'right') {
+    carrocelContent.scrollLeft += 100;
+
+  }
 }
